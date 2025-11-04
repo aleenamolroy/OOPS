3ab222bb5ab6932a9400eeca5de51aadf00852fa
aleenamolroy <aleenamolroy02@gmail.com>
2025-11-04 16:05:42 +0530

    polymorphism

--- polymorphism.js
+++ polymorphism.js
@@ -1,1 +1,26 @@
- clad
+class shape{
+    draw(){
+        console.log("draw a shape..");
+        
+    }
+}
+class circle extends shape{
+    draw(){
+        console.log("Drawing circle..")
+    }
+}
+class Rectangle extends shape{
+    draw(){
+        console.log('Drawing Rectangle..');
+        
+    }
+}
+class triangle extends shape{
+    draw(){
+        console.log('Drawing Triangle..')
+    }
+}
+const shapes=[new circle(),new Rectangle(),new triangle()]
+for(let s of shapes){
+    s.draw()
+}
